bdf263114def5031fd406132609abd23dd63560c
himanjanpati <himanjan.pati@outlook.com>
2022-05-29 01:33:47 +0000

    corrected bucket url

--- sam-demo/commands.sh
+++ sam-demo/commands.sh
@@ -1,6 +1,6 @@
 #create an S3 bucket
 aws s3 mb s3://himanjan-code-sam
 
-aws cloudformation package --s3-bucket s3://himanjan-code-sam --template-file template.yaml --output-template-file gen/template-generated.yaml
+aws cloudformation package --s3-bucket himanjan-code-sam --template-file template.yaml --output-template-file gen/template-generated.yaml
 
 aws cloudformation deploy --template-file gen/template-generated.yaml --stack-name hello-world-sam --capabilities CAPABILITY_IAM
